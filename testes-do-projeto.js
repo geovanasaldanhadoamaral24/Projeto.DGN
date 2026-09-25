be35f8a04cba03f6214a195606ab48a44992c2a0
geovanasaldanhadoamaral24 <geovanasaldanhadoamaral24@gmail.com>
2026-09-25 03:00:06 +0000

    testes feitos

---
+++ testes-do-projeto.js
@@ -0,0 +1,19 @@
+// 1 - Testar a rota inicial
+//GET http://localhost:3000/
+
+// 2 - Listar todas as tarefas
+//GET http://localhost:3000/tarefas
+
+// 3 - Buscar tarefa pelo ID 1
+//GET http://localhost:3000/tarefas/1
+
+// 4 - Filtrar tarefas concluídas
+//GET http://localhost:3000/tarefas?concluida=true
+
+// 5 - Adicionar uma nova tarefa
+//POST http://localhost:3000/tarefas
+//Content-Type: application/json
+
+//{
+  //  "titulo": "Ler um livro"
+//}
